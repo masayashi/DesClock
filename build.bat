b504d2463c72907827d8bf6f2c07eec0cc7554bd
masayashi <masayashi@gmail.com>
2019-01-25 00:52:57 +0900

    パッケージ化のための調整

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set command=npx electron-packager ./src desclock --platform=win32 --arch=x64
+set command=npx electron-packager ./src desclock --platform=win32 --arch=x64 --asar --out package
 echo %command%
 %command%
 

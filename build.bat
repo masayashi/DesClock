0d6a709480c948750dfb392775f4ec3213a30892
masayashi <masayashi@gmail.com>
2019-02-03 11:37:23 +0900

    Add icon

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set command=npx electron-packager ./src desclock --platform=win32 --arch=x64 --asar --out package
+set command=npx electron-packager ./src desclock --platform=win32 --arch=x64 --asar --out package --overwrite --icon=resources/icon.ico
 echo %command%
 %command%
 
